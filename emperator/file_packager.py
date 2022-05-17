6eacefa560be3decd0aef31078df2a2028a86479
DerThorsten <derthorstenbeier@gmail.com>
2022-05-17 16:11:28 +0200

    filepacker for individual pkgs

--- emperator/file_packager.py
+++ emperator/file_packager.py
@@ -5,6 +5,18 @@ from pathlib import Path
 import shutil
 import tempfile
 import typer
+import glob
+import json
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 
 def ensure_python(env_prefix: Path, version: str):
@@ -27,8 +39,13 @@ def get_file_packager_path():
     current_conda_env = os.environ.get("CONDA_PREFIX")
     if current_conda_env is not None:
         conda_file_packager = (
-            Path(current_conda_env) / "opt" / "emsdk" / "upstream" /
-            "emscripten" / "tools" / "file_packager.py"
+            Path(current_conda_env)
+            / "opt"
+            / "emsdk"
+            / "upstream"
+            / "emscripten"
+            / "tools"
+            / "file_packager.py"
         )
         if os.path.isfile(conda_file_packager):
             return conda_file_packager
@@ -123,6 +140,86 @@ def pack_file(
             mount_path=mount_path,
             export_name=export_name,
             use_preload_plugins=use_preload_plugins,
+            no_node=export_name.startswith("globalThis"),
+            lz4=True,
+            cwd=temp_dir_str,
+        )
+
+        shutil.copy(os.path.join(temp_dir_str, f"{outname}.data"), os.getcwd())
+        shutil.copy(os.path.join(temp_dir_str, f"{outname}.js"), os.getcwd())
+
+
+def create_env(pkg_name, prefix, platform):
+    # cmd = ['$MAMBA_EXE' ,'create','--prefix', prefix,'--platform=emscripten-32'] + [pkg_name] #+ ['--dryrun']
+    cmd = [
+        f"$MAMBA_EXE install --yes --prefix {prefix} --platform={platform} --no-deps {pkg_name}"
+    ]
+    ret = subprocess.run(cmd, shell=True)
+    returncode = ret.returncode
+    assert returncode == 0
+
+
+def get_pkg_info(pkg_name, prefix):
+
+    # load *.json from prefix/conda-meta
+    meta_path = os.path.join(prefix, "conda-meta", "*.json")
+    pkg_jsons = glob.glob(meta_path)
+    print(pkg_jsons)
+    assert len(pkg_jsons) == 1
+    pkg_json = pkg_jsons[0]
+
+    with open(pkg_json) as json_file:
+        data = json.load(json_file)
+        # print(data)
+    path = data["package_tarball_full_path"]
+    head, tail = os.path.split(path)
+    return tail[: -len(".tar.bz2")]
+
+
+def pack_conda_pkg(
+    pkg_name,
+    channels,
+    prefix,
+    target_platform,
+    override=False,
+    filter=None,
+):
+
+    # create the env which only contains the pkg but not
+    # the dependencies
+    create_env(pkg_name, prefix, platform=target_platform)
+
+    pkg_full_name = get_pkg_info(pkg_name, prefix)
+    print(pkg_full_name)
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir_str = str(temp_dir)
+
+        ignore = shutil.ignore_patterns(
+            "*.pyc",
+            "*.o",
+            "*.saa",
+            "Makefile",
+            "*.wasm",
+            "*cpython-311.data",
+            "*.a",
+            "*.zip",
+            "*.tar.gz",
+        )
+        # ignore=None
+        print(f"copy tree from  {prefix} to {temp_dir_str}")
+        d = os.path.join(temp_dir_str, "the_env")
+        copytree(prefix, temp_dir_str, ignore=ignore)
+
+        mount_path = prefix
+        export_name = "globalThis.Module"
+        outname = pkg_full_name
+        emscripten_file_packager(
+            outname=outname,
+            to_mount=prefix,
+            mount_path=prefix,
+            export_name=export_name,
+            use_preload_plugins=True,
             no_node=export_name.startswith("globalThis"),
             lz4=True,
             cwd=temp_dir_str,
