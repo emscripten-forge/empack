6c7eeb6937f7ab98b190e1bbf4ccb3877784dda7
Thorsten Beier <derthorstenbeier@gmail.com>
2022-05-17 09:47:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 requirements = [
