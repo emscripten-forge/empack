4109517d587dc3224e8ad6485e39b69df2faac7f
DerThorsten <derthorstenbeier@gmail.com>
2022-05-18 13:19:32 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -6,53 +6,47 @@ import sys
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = [
-    'typer'
-]
+requirements = ["typer"]
 
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup_requirements = [
     #'pytest-runner'
 ] + pytest_runner
 
-test_requirements = [
-    'pytest',
-    'coverage',
-    'flake8'
-]
+test_requirements = ["pytest", "coverage", "flake8"]
 
 setup(
     author="Thorsten Beier",
-    author_email='derthorstenbeier@gmail.com',
+    author_email="derthorstenbeier@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="empack emscripten+boa",
     entry_points={
-        'console_scripts': [
-            'empack=empack.cli.main:app',
+        "console_scripts": [
+            "empack=empack.cli.main:app",
         ],
     },
     install_requires=requirements,
     long_description=readme,
     include_package_data=True,
-    keywords='empack',
-    name='empack',
+    keywords="empack",
+    name="empack",
     packages=find_packages(),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/emscripten-forge/empack',
-    version='0.2.0',
+    url="https://github.com/emscripten-forge/empack",
+    version="0.3.0",
     zip_safe=False,
 )
