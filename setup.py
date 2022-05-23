a4bb3059589f523b98ae43fbda2c49f543e1090a
martinRenou <martin.renou@gmail.com>
2022-05-23 17:23:39 +0200

    Release 0.5.3

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/emscripten-forge/empack",
-    version="0.5.2",
+    version="0.5.3",
     zip_safe=False,
 )
