45800a005694c9e1efc64daad2bde9ab981af240
DerThorsten <derthorstenbeier@gmail.com>
2022-06-20 10:16:39 +0200

    bumped version hotfix

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/emscripten-forge/empack",
-    version="0.5.4",
+    version="0.6.0",
     zip_safe=False,
 )
