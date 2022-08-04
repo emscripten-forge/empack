2732211e989b82fe406655cbd7984f0797759a3b
DerThorsten <derthorstenbeier@gmail.com>
2022-08-04 16:50:55 +0200

    added missing file

---
+++ empack/sort_packed.py
@@ -0,0 +1,26 @@
+import json
+
+
+def sort_packed(in_file_path, out_file_path):
+    lines = []
+    with open(in_file_path, "r") as in_file:
+        for line in in_file:
+            if 'loadPackage({"files":' in line:
+                files = eval(line[16:-3])["files"]
+                files.sort(key=lambda x: x["filename"])
+                for file in files:
+                    print(file)
+                line = f"""loadPackage({
+                    json.dumps({"files": files})
+                });"""
+            lines.append(line)
+    with open(out_file_path, "wt") as out_file:
+        for line in lines:
+            out_file.write(line)
+
+
+if __name__ == "__main__":
+
+    in_file_path = "/home/derthorsten/src/empack/python_dataa.js"
+    out_file_path = "/home/derthorsten/src/empack/sorted_python_dataa.js"
+    sort_packed(in_file_path=in_file_path, out_file_path=out_file_path)
