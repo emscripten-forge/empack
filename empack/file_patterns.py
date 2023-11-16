10a3e73bc476ecde6cb3d7d8a442eee509dc92f4
DerThorsten <derthorstenbeier@gmail.com>
2023-11-16 12:32:32 +0100

    fixed stuff

--- empack/file_patterns.py
+++ empack/file_patterns.py
@@ -9,8 +9,6 @@ class RegexPattern:
         self._pattern = re.compile(regex)
 
     def match(self, path):
-        if not hasattr(self, "_pattern") or self._pattern:
-            self._pattern = re.compile(self.regex)
         return self._pattern.match(path) is not None
 
 
