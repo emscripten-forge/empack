da898d92e619d887099f7de46bd7078b44ae7e4a
DerThorsten <derthorstenbeier@gmail.com>
2024-01-02 12:00:31 +0100

    add/append file to exiting packaged meta

--- empack/cli/pack.py
+++ empack/cli/pack.py
@@ -4,7 +4,13 @@ from typing import Optional
 import typer
 
 from empack.file_patterns import pkg_file_filter_from_yaml
-from empack.pack import DEFAULT_CONFIG_PATH, pack_env
+from empack.pack import (
+    DEFAULT_CONFIG_PATH,
+    add_tarfile_to_env_meta,
+    pack_directory,
+    pack_env,
+    pack_file,
+)
 
 from .app import app
 
@@ -70,3 +76,94 @@ def pack_env_cli(
         use_cache=use_cache,
         compresslevel=compresslevel,
     )
+
+
+# pack directory
+@pack_app.command(name="dir", help="""Pack a directory into a tarball:""")
+def pack_dir_cli(
+    host_dir: Path = typer.Option(  # noqa: B008
+        ...,
+        "--host-dir",
+        "-d",
+        help="path of the directory in host filesystem",
+    ),
+    mount_dir: Path = typer.Option(  # noqa: B008
+        ...,
+        "--mount-dir",
+        "-m",
+        help="path of the directory in the the virtual filesystem",
+    ),
+    outname: Path = typer.Option(  # noqa: B008
+        ...,
+        "--outname",
+        "-o",
+        help="name of the output tarball",
+    ),
+    outdir: Path = typer.Option(  # noqa: B008
+        None,
+        "--outdir",
+        "-o",
+        help="if no output directory is specified the current workdir is used",
+    ),
+):
+    pack_directory(
+        host_dir=host_dir,
+        mount_dir=mount_dir,
+        outname=outname,
+        outdir=outdir,
+    )
+
+
+# pack file
+@pack_app.command(name="file", help="""Pack a directory into a tarball:""")
+def pack_file_cli(
+    host_file: Path = typer.Option(  # noqa: B008
+        ...,
+        "--host-file",
+        "-d",
+        help="path of the file in host filesystem",
+    ),
+    mount_dir: Path = typer.Option(  # noqa: B008
+        ...,
+        "--mount-dir",
+        "-m",
+        help="path of the directory in the the virtual filesystem",
+    ),
+    outname: Path = typer.Option(  # noqa: B008
+        ...,
+        "--outname",
+        "-o",
+        help="name of the output tarball",
+    ),
+    outdir: Path = typer.Option(  # noqa: B008
+        None,
+        "--outdir",
+        "-o",
+        help="if no output directory is specified the current workdir is used",
+    ),
+):
+    pack_file(
+        host_file=host_file,
+        mount_dir=mount_dir,
+        outname=outname,
+        outdir=outdir,
+    )
+
+
+# new command to append to existing tarballs
+@pack_app.command(name="append", help="""Append tarballs to a packaged environment""")
+def append_to_env_cli(
+    env_meta: Path = typer.Option(  # noqa: B008
+        ...,
+        "--env-meta",
+        "-e",
+        help="path of the env meta file (or the directory containing it)",
+    ),
+    tarfile: Path = typer.Option(  # noqa: B008
+        ...,
+        "--tarfile",
+        "-t",
+        help="path of the tarfile to append",
+    ),
+):
+    add_tarfile_to_env_meta(env_meta_filename=env_meta, tarfile=tarfile)
