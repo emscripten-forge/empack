3848ac3af904032179e3e2b9b1d7253894c145ab
martinRenou <martin.renou@gmail.com>
2024-02-01 11:22:36 +0100

    Show a bit more than a return code

--- empack/micromamba_wrapper.py
+++ empack/micromamba_wrapper.py
@@ -58,11 +58,7 @@ def create_environment(
         print(" ".join(micromamba_command))
         return
 
-    try:
-        extra_kwargs = {}
-        if supress_stdout:
-            extra_kwargs["stdout"] = subprocess.DEVNULL
-        subprocess.run(micromamba_command, check=True, **extra_kwargs)  # noqa:  #  noqa: S603
-    except subprocess.CalledProcessError as e:
-        error_message = f"Error: Micromamba command failed with return code {e.returncode}"
-        raise Exception(error_message) from e
+    extra_kwargs = {}
+    if supress_stdout:
+        extra_kwargs["stdout"] = subprocess.DEVNULL
+    subprocess.run(micromamba_command, check=True, **extra_kwargs)  # noqa:  #  noqa: S603
